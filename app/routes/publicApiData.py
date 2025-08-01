e86a5eb38d9995885801396ffe3783f2968a8818
SanKang <84171004+rkdtks1997@users.noreply.github.com>
2025-08-01 17:15:48 +0900

    test

--- app/routes/publicApiData.py
+++ app/routes/publicApiData.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, HTTPException
 import requests
 import os
 from app.utils.commonutil import get_salesforce_token
-from app.services.salesforce import send_to_salesforce
+from app.utils.commonutil import sf_post
+
 router = APIRouter()
 
 SUBWAY_API_KEY = os.environ.get("SUBWAY_API_KEY")
@@ -17,7 +18,20 @@ def get_subway_data():
         return response.json()
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail=f"[Subway API Error] {str(e)}")
+    
+def send_to_salesforce(path: str, payload: dict):
+    """Salesforce에 POST 요청을 보내는 공통 함수"""
+    try:
+        token_data = get_salesforce_token()
+        access_token = token_data["access_token"]
+        instance_url = token_data["instance_url"]
 
+        # 내부 Salesforce POST 유틸 사용
+        response = sf_post(path, payload, access_token, instance_url)
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"[Salesforce API Error] {str(e)}")
+    
 @router.post("/sf-subway-proxy")
 async def sf_subway_proxy():
     try:
--- app/services/salesforce.py
+++ app/services/salesforce.py
@@ -27,16 +27,3 @@ def create_interface(data: Interface_In):
     res = sf_post("sobjects/InterfaceData__c", payload, access_token, instance_url)
     print("res",res)
     return res.json()
-
-def send_to_salesforce(path: str, payload: dict):
-    """Salesforce에 POST 요청을 보내는 공통 함수"""
-    try:
-        token_data = get_salesforce_token()
-        access_token = token_data["access_token"]
-        instance_url = token_data["instance_url"]
-
-        # 내부 Salesforce POST 유틸 사용
-        response = sf_post(path, payload, access_token, instance_url)
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(status_code=500, detail=f"[Salesforce API Error] {str(e)}")
