da28fe7b3fb6ecf16e9170b28118848a5ce7d823
SanKang <84171004+rkdtks1997@users.noreply.github.com>
2025-08-04 12:37:03 +0900

    test

--- app/routes/publicApiData.py
+++ app/routes/publicApiData.py
@@ -98,7 +98,7 @@ def get_movie_data():
         if not all([MOVIE_KEY, MOVIE_URL]):
             raise ValueError("환경변수 설정이 누락되었습니다.")
 
-        targetDt = datetime.now().strftime("%Y%m%d")  # 혹은 프론트에서 받은 키워드 등으로 동적으로 구성
+        targetDt = datetime.now().month(-1).strftime("%Y%m%d")  # 혹은 프론트에서 받은 키워드 등으로 동적으로 구성
 
         url = f"{MOVIE_URL}?key={MOVIE_KEY}&targetDt={targetDt}"
         print(f"🔍 요청 URL: {url}")
